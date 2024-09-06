f8f02ac20e757c59e3cad02e044e281f4bd99994
Paul Richard <paulrpm@gmail.com>
2024-09-06 08:42:04 -0300

    created: created user listing request class

---
+++ app/app/Http/ApiHandler/Methods/ListUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\ApiHandler\Methods;
+
+use App\Http\ApiHandler\TaskApi;
+use Exception;
+
+class ListUserRequest extends TaskApi
+{
+    protected $methodUrl = '/api/users/list';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function execute($params = [])
+    {
+        try{
+            $response = $this->handleRequest('get', $this->methodUrl, [], '', $params);
+            self::logRequest("Request successful: Retrieved " . $response['data']['total'] . " users");
+    
+            $userIds = array_column($response['data']['users'], 'id');
+            if (count($userIds) > 0) {
+                return $userIds[array_rand($userIds)];
+            }
+            
+            self::logRequest("Request finished with no changes: No users found ");
+            return null;
+        } catch (Exception $ex) {
+            $exception = ($ex->getMessage()) ?? 'Check api log';
+            self::logRequest("Request failed: " . $exception);
+
+            if ($ex->getCode() == 401) {
+                self::retrieveToken(true);
+            }
+        }
+    }
+}
